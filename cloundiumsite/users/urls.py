60b74d237e5cc652be10215f36a314d6c3c60ed7
NishanthMuruganantham <nishanthmurugananth10@gmail.com>
2021-07-29 01:09:39 +0530

    password reset view has been added

--- cloundiumsite/users/urls.py
+++ cloundiumsite/users/urls.py
@@ -6,8 +6,33 @@ app_name = 'users'
 
 urlpatterns = [
     # path('login/', views.user_login, name = 'user_login'),
+    path('login/', views.LoginView.as_view(), name = 'user_login'),
     path('logout/', auth_views.LogoutView.as_view(), name = 'user_logout'),
     path('signup/',views.user_signup, name = 'user_signup'),
-    path('activate/<uidb64>/<token>', views.activate_user, name='activate'),
-    path('login/', views.LoginView.as_view(), name = 'user_login'),
+    path('activate/<uidb64>/<token>/', views.activate_user, name='activate'),
+    path('change-password/', views.PasswordChangeView.as_view(), name='change_password'),
+    
+    path(
+        'change-password-mail-sent/',
+        auth_views.PasswordResetDoneView.as_view(
+            template_name = 'users/password/password_reset_mail_sent_notification.html'
+        ),
+        name = 'change_password_mail_sent'
+    ), 
+    
+    path(
+        'confirm-password-reset/<uidb64>/<token>/',
+        auth_views.PasswordResetConfirmView.as_view(
+            template_name = 'users/password/password_reset_confirmation.html'    
+        ),
+        name = 'confirm_password_reset'
+    ),
+    
+    path(
+        'password-reset-completed/',
+        auth_views.PasswordResetCompleteView.as_view(
+            template_name = 'users/password/password_reset_complete.html'
+        ),
+        name = 'password_reset_complete'
+    ),
 ]
--- cloundiumsite/users/views.py
+++ cloundiumsite/users/views.py
@@ -6,12 +6,14 @@ from django.http import HttpResponse
 from django.contrib.auth import get_user_model
 from django.views import View
 from django.contrib.auth import views as auth_views
+from django.contrib.auth.forms import PasswordResetForm
 
 from django.contrib.sites.shortcuts import get_current_site
 from django.template.loader import render_to_string
 from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
 from django.utils.encoding import force_bytes, force_text
 from django.core.mail import EmailMessage
+from django.contrib.auth.tokens import default_token_generator
 
 from .forms import LoginForm, SignupForm
 from .tokens import account_activation_token
@@ -54,6 +56,25 @@ def activate_user(request, uidb64, token):
         return render(request, 'users/user_activation_link_success.html')
     else:
         return HttpResponse('Activation link is invalid!')
+
+
+#* FOR SENDING PASSWORD RESET MAIL TO THE USER
+def send_password_reset_mail(request, user, user_email):
+    current_site = get_current_site(request)
+    mail_subject = f"Reset Your Password for {current_site}"
+    message = render_to_string(
+        'users/password/password_reset_link_email.html',{
+            'user'  : user,
+            'domain': current_site.domain,
+            'uid'   : urlsafe_base64_encode(force_bytes(user.id)),
+            'token' : default_token_generator.make_token(user),
+        }
+    )
+    to_email = user_email
+    email = EmailMessage(
+        mail_subject, message, to=[to_email]
+    )
+    email.send()
 
 
 #* CUSTOM LOGIN VIEW
@@ -101,3 +122,29 @@ def user_signup(request):
         form = SignupForm()
     return render(request, 'users/registration.html', {'form': form})
 
+
+class PasswordChangeView(View):
+    template_name = 'users/password/password_reset.html'
+    form_class = PasswordResetForm
+    
+    def post(self, request):
+        password_reset_form = self.form_class(request.POST)
+        if password_reset_form.is_valid():
+            email_data = password_reset_form.cleaned_data['email']
+            try:
+                associated_user = User.objects.get(email__exact = email_data)
+            except User.DoesNotExist:
+                messages.error(request,'No user found for this email address')
+                return render(request, self.template_name, {'form': password_reset_form})
+            if associated_user:
+                send_password_reset_mail(request,user = associated_user,user_email = email_data)
+                return redirect(reverse('users:change_password_mail_sent'))
+            else:
+                messages.error(request,'No user found for this email address')
+                return render(request, self.template_name, {'form': password_reset_form})
+        return render(request, self.template_name, {'form': password_reset_form})
+    
+    
+    def get(self, request):
+        password_reset_form = PasswordResetForm()
+        return render(request, self.template_name, {'form': password_reset_form})
